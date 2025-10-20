341f094727275c79e386c8ccbd5215ba8ba44ce7
Oleg <sleite4r@gmail.com>
2025-10-20 21:51:18 +0200

    Refactor Redux setup: update contactsOps, contactsSlice, store and install required dependencies (axios, @reduxjs/toolkit, react-redux, reselect) with _id support

---
+++ front-end/src/redux/contactsOps.js
@@ -0,0 +1,59 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const BASE_URL = "http://localhost:3000"; // backend
+
+//Get all contacts
+export const fetchContacs = createAsyncThunk(
+  "contacts/fetchAll",
+  async (_, thunkAPI) => {
+    try {
+      const res = await axios.get(`${BASE_URL}/get-phone`);
+      return res.data.data.phoneNumber; // array of contact
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+//Add new contact
+export const addContact = createAsyncThunk(
+  "contacts/addContact",
+  async (contact, thunkAPI) => {
+    try {
+      const res = await axios.post(`${BASE_URL}/add-phone`, contact);
+      return res.data.data.phoneNumber;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+//Update contact use the _id
+export const updateContact = createAsyncThunk(
+  "contacts/updateContact",
+  async ({ id, updateData }, thunkAPI) => {
+    try {
+      const res = await axios.patch(
+        `${BASE_URL} / update - phone / ${id}`,
+        updateData
+      );
+      return res.data.data.updatePhone; //Update Contact
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+//Update contact use the _id
+export const deleteContact = createAsyncThunk(
+  "contacts/deleteContact",
+  async (id, thunkAPI) => {
+    try {
+      await axios.delete(`${BASE_URL}/delete-phone/${id}`);
+      return id;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
