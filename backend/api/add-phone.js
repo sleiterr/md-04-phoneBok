8a2431e3b8e5b2d044ef3f256127041803bc1534
Oleg <sleite4r@gmail.com>
2025-11-25 21:37:25 +0100

    update

--- backend/api/add-phone.js
+++ backend/api/add-phone.js
@@ -1,26 +1,22 @@
-import { connectDB } from "./db";
-import PhoneBook from "../models/my_contact";
+import mongoose from "mongoose";
+import PhoneBook from "../../backend/models/my_contact";
+
+mongoose.connect(process.env.URI);
 
 export default async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ message: "Method not allowed" });
   }
-
-  await connectDB();
-
   try {
-    let { phoneNumber, name } = req.body;
-
-    if (!phoneNumber.startsWith("+")) {
-      phoneNumber = `+${phoneNumber.replace(/[\s-]/g, "")}`;
+    const body = req.body;
+    if (!body.phoneNumber.startsWith("+")) {
+      body.phoneNumber = `+${body.phoneNumber.replace(/[\s-]/g, "")}`;
     }
-
-    const newPhone = await PhoneBook.create({ phoneNumber, name });
-
-    res.status(201).json({
-      status: "Success",
-      data: { phoneNumber: newPhone },
-    });
+    const newContact = new PhoneBook(body);
+    await newContact.save();
+    res
+      .status(201)
+      .json({ status: "Success", data: { phoneNumber: newContact } });
   } catch (err) {
     res.status(500).json({ status: "Failed", message: err.message });
   }
